a3a16ca089d12c9b2622e15b493b7a4efd19eb8b
Nurlybek123456 <200103414@stu.sdu.edu.kz>
2022-01-03 00:42:11 +0500

    make it better

---
+++ myp3.py
@@ -0,0 +1,75 @@
+import telebot
+bot = telebot.TeleBot("5056731306:AAHZDKfF-Kp1f8qid8AilyWQ_OOuW-Mt6Mk")
+from time import sleep
+
+
+
+ADMIN_ID = "805509116"
+
+
+
+@bot.message_handler(commands=['приветсестракотораяцундерекотораяединственнаяуменя'])
+def sends(message):
+    msg = bot.reply_to(message,"☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁ \n \n \n \n \n \n \n⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️")
+    sleep(0.2)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text='☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁   \n  ❄️      ❄️    ❄️  ❄️      ❄️  ❄️ \n \n \n \n \n \n⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄  ')
+    sleep(0.2)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text='☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁   \n  ❄️      ❄️    ❄️  ❄️      ❄️  ❄️\n❄️      ❄️    ❄️  ❄️      ❄️  ❄\n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄️️ \n \n \n \n⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄  ')
+    sleep(0.2)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text='☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁   \n  ❄️      ❄️    ❄️  ❄️      ❄️  ❄️\n❄️      ❄️    ❄️  ❄️      ❄️  ❄\n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄\n❄️      ❄️    ❄️  ❄️      ❄️  ❄ \n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄️️ \n \n⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄  ')
+    sleep(1)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text='☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁   \n  ❄️      ❄️    ❄️  ❄️      ❄️  ❄ ️\n❄️      ❄️    ❄️  ❄️      ❄️  ❄\n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄\n❄️      ❄️    ❄️  ❄️      ❄️  ❄ \n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄️️ \n \nП️☃️⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄  ')
+    sleep(0.2)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text='☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁   \n  ❄️      ❄️    ❄️  ❄️      ❄️  ❄ ️\n❄️      ❄️    ❄️  ❄️      ❄️  ❄\n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄\n❄️      ❄️    ❄️  ❄️      ❄️  ❄ \n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄️️ \n \nП Р И️⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄  ')
+    sleep(0.1)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text='☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁   \n    💥🎉🥳💥🎉\n❄️      ❄️    ❄️  ❄️      ❄️  ❄\n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄\n❄️      ❄️    ❄️  ❄️      ❄️  ❄ \n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄️️ \n \nП Р И В Е Т⛄️☃️⛄️☃️⛄️☃️⛄  ')
+    sleep(0.2)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text='☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁   \n    💥🎉🥳💥🎉🎈🎁🎊💥\n❄️      ❄️    ❄️  ❄️      ❄️  ❄\n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄\n❄️      ❄️    ❄️  ❄️      ❄️  ❄ \n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄️️ \n \nП Р И В Е Т  ️СЕ️⛄️☃️⛄️☃️⛄  ')
+    sleep(0.2)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text='☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁   \n    💥🎉🥳💥🎉🎈🎁🎊💥\n\n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄\n❄️      ❄️    ❄️  ❄️      ❄️  ❄ \n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄️️ \n \nП Р И В Е Т  ️С Е С Т☃️⛄  ')
+    sleep(0.2)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text='☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁   \n    💥🎉🥳💥🎉🎈🎁🎊💥\n\n\n❄️      ❄️    ❄️  ❄️      ❄️  ❄ \n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄️️ \n \nП Р И В Е Т  ️С Е С Т Р А   ')
+    sleep(0.2)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text='☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁   \n    💥🎉🥳💥🎉🎈🎁🎊💥\n\n\n\n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄️️ \n \n   🎉   П Р И В Е Т  ️С Е С Т Р А    ')
+    sleep(0.2)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text='☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁   \n    💥🎉🥳💥🎉🎈🎁🎊💥\n\n\n\n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄️️ \n \n 🎉   П Р И В Е Т  ️С Е С Т Р А   🎉')
+
+@bot.message_handler(commands=['snow'])
+def sends(message):
+    msg = bot.reply_to(message,"☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁ \n \n \n \n \n \n \n⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️")
+    sleep(0.2)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text='☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁   \n  ❄️      ❄️    ❄️  ❄️      ❄️  ❄️ \n \n \n \n \n \n⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄  ')
+    sleep(0.2)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text='☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁   \n  ❄️      ❄️    ❄️  ❄️      ❄️  ❄️\n❄️      ❄️    ❄️  ❄️      ❄️  ❄\n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄️️ \n \n \n \n⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄  ')
+    sleep(0.2)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text='☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁   \n  ❄️      ❄️    ❄️  ❄️      ❄️  ❄️\n❄️      ❄️    ❄️  ❄️      ❄️  ❄\n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄\n❄️      ❄️    ❄️  ❄️      ❄️  ❄ \n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄️️ \n \n⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄  ')
+    sleep(1)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text='☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁   \n  ❄️      ❄️    ❄️  ❄️      ❄️  ❄ ️\n❄️      ❄️    ❄️  ❄️      ❄️  ❄\n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄\n❄️      ❄️    ❄️  ❄️      ❄️  ❄ \n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄️️ \n \nH️☃️⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄  ')
+    sleep(0.2)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text='☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁   \n  ❄️      ❄️    ❄️  ❄️      ❄️  ❄ ️\n❄️      ❄️    ❄️  ❄️      ❄️  ❄\n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄\n❄️      ❄️    ❄️  ❄️      ❄️  ❄ \n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄️️ \n \nH ️A️⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄  ')
+    sleep(0.1)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text='☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁   \n    💥🎉🥳💥🎉\n❄️      ❄️    ❄️  ❄️      ❄️  ❄\n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄\n❄️      ❄️    ❄️  ❄️      ❄️  ❄ \n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄️️ \n \nH ️A ️P ️P️⛄️☃️⛄️☃️⛄️☃️⛄  ')
+    sleep(0.2)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text='☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁   \n    💥🎉🥳💥🎉🎈🎁🎊💥\n❄️      ❄️    ❄️  ❄️      ❄️  ❄\n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄\n❄️      ❄️    ❄️  ❄️      ❄️  ❄ \n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄️️ \n \nH ️A ️P ️P ️Y   ️☃️⛄️☃️⛄️☃️⛄  ')
+    sleep(0.2)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text='☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁   \n    💥🎉🥳💥🎉🎈🎁🎊💥\n\n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄\n❄️      ❄️    ❄️  ❄️      ❄️  ❄ \n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄️️ \n \nH ️A ️P ️P ️Y    ️N E️☃️⛄️☃️⛄  ')
+    sleep(0.2)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text='☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁   \n    💥🎉🥳💥🎉🎈🎁🎊💥\n\n\n❄️      ❄️    ❄️  ❄️      ❄️  ❄ \n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄️️ \n \nH ️A ️P ️P ️Y    ️N E W    ️⛄️☃️⛄  ')
+    sleep(0.2)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text='☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁   \n    💥🎉🥳💥🎉🎈🎁🎊💥\n\n\n\n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄️️ \n \n   🎉   H ️A ️P ️P ️Y    ️N E W    D️⛄  ')
+    sleep(0.2)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text='☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁   \n    💥🎉🥳💥🎉🎈🎁🎊💥\n\n\n\n    ❄️      ❄️    ❄️  ❄️      ❄️  ❄️️ \n \n 🎉   H ️A ️P ️P ️Y    ️N E W    D A Y 🎉')
+
+@bot.message_handler(content_types=['text'])
+def send(message):
+   if message.text.lower()== 'ау':
+    bot.reply_to(message, 'Нанпалааау🤣')
+   elif message.text.lower()=='au':
+    bot.reply_to(message, 'Nanpalaaau🤣')
+   elif message.text == '-смс' and message.from_user.id==992130415:
+    bot.delete_message(message.chat.id, message.reply_to_message.id)
+    bot.delete_message(message.chat.id,message.id)
+   elif message.from_user.id==2106692657:
+       bot.delete_message(message.chat.id, message.id)
+
+bot.infinity_polling()
